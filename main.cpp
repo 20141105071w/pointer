96a79a1af671a008f01f00c9f646def6d539d77f
20141105071 <20141105071wyr@mac26.local>
2015-12-02 21:16:20 +0800

    pointer
    定义一个整型变量和int类型指针；将100赋值给啊a，将a的地址赋值给指针变量p，输出a的地址，p的地址，p所指向的地址，p的地址所赋的值。

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  1
+//
+//  Created by 20141105071 on 15/11/11.
+//  Copyright © 2015年 20141105071. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int a,*p;
+    a=100;
+    p=&a;
+    printf("a=%x &a=%x  &p=%x p=%x  *p=%x\n",a,&a,&p,p,*p);
+    return 0;
+}
